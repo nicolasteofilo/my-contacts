d6c6bf0e2994477f7b7416334b610ff70908f762
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-15 09:52:19 -0300

    chore(api): add eslint and editor config

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello Word" });
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello Word' });
 });
 
 const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`🔥 Sever started at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`🔥 Sever started at http://localhost:${PORT}`));
