58c04d66c8d32ca7fe7516d8f82beb2dbbb3a215
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-18 21:43:44 -0300

    feat: implementation for DELETE category

--- api/src/app/controllers/CategoryController.js
+++ api/src/app/controllers/CategoryController.js
@@ -60,6 +60,12 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await CategoriesRepository.delete(id);
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoryController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -17,5 +17,6 @@ router.get('/categories', CategoryController.index);
 router.post('/categories/', CategoryController.store);
 router.get('/categories/:id', CategoryController.show);
 router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
