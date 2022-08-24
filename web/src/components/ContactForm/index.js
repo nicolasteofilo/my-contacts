96822dd2e4ecffc5ad251c7dee7a22a00093686e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-23 22:31:03 -0300

    feat(web): getting the form data

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
@@ -8,20 +9,52 @@ import Button from '../Button';
 import { Form, ButtonContainer } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(event) => handleSubmit(event)}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-      <FormGroup error="O formato do e-mail é inválido">
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-      <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="linkedin">Linkedin</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
