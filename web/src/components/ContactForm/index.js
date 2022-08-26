36e640d05318591b365a3c57650d32ee2c5dfe2c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-25 21:58:40 -0300

    feat(web): email field validation

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { useState } from 'react';
+import isEmailValid from '../../utils/isEmailValid';
+
+import { Form, ButtonContainer } from './styles';
+
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
-
-import { Form, ButtonContainer } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
@@ -31,6 +33,26 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find(
+        (error) => error.field === 'email'
+      );
+      if (!errorAlreadyExists) {
+        setErrors((prevState) => [
+          ...prevState,
+          { field: 'email', message: 'E-mail inválido' },
+        ]);
+      }
+    } else {
+      setErrors((prevState) =>
+        prevState.filter((error) => error.field !== 'email')
+      );
+    }
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log({
@@ -50,7 +72,7 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
         />
       </FormGroup>
       <FormGroup>
