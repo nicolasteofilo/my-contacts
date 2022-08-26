28c2fb8cec56048515288848f4d17a7eae6f46ee
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-08-25 22:41:53 -0300

    feat(web): useError custon hook

---
+++ web/src/hooks/useErrors.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export default function useErrors() {
+  const [errors, setErrors] = useState([]);
+
+  function setError({ field, message }) {
+    const errorAlreadyExists = errors.find((error) => error.field === field);
+    if (!errorAlreadyExists) {
+      setErrors((prevState) => [...prevState, { field, message }]);
+    }
+  }
+
+  function removeError(fieldName) {
+    setErrors((prevState) =>
+      prevState.filter((error) => error.field !== fieldName)
+    );
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  return { setError, removeError, getErrorMessageByFieldName };
+}
