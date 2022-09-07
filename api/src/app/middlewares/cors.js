0e439c59fa50c9ab7614a0120e3f3487798ae3b0
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-09-06 23:23:48 -0300

    feat(api): allow cache in preflight

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -2,5 +2,6 @@ module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Allow-Age', '10');
   next();
 }
