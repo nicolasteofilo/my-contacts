93543f22952819c1a173741f5206f702c31431ac
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-09-12 21:56:52 -0300

    feat(web): render contacts list in Home page

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -13,13 +14,25 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch(() => {});
+  }, []);
   return (
     <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length} {contacts.length === 1 ? 'contatos' : 'contato'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
@@ -34,39 +47,30 @@ export default function Home() {
           </button>
         </header>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Mateus Silva</strong>
-              <small>instagram</small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && (
+                  <small>{contact.category_name}</small>
+                )}
+              </div>
+              {contact.email && <span>{contact.email}</span>}
+              <span>{contact.phone}</span>
             </div>
-            <span>mateus@teste.com</span>
-            <span>(41) 9999-9999</span>
-          </div>
 
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Editar" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Editar" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Deletar" />
+              </button>
+            </div>
+          </Card>
+        ))}
       </ListContainer>
     </Container>
   );
 }
-
-fetch('http://localhost:3001/contacts', {
-  method: 'DELETE',
-  headers: new Headers({
-    'X-App-ID': '123',
-  }),
-})
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
