9d7c9b63c88b24080f7f6cc8d4ac03881cdc1a91
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-09-27 18:02:21 -0300

    feat(web): create ContactsService to load contacts list

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -14,6 +14,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
+import ContactsService from '../../service/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -34,9 +35,8 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-        const json = await response.json();
-        setContacts(json);
+        const contactsList = await ContactsService.listContacts();
+        setContacts(contactsList);
       } catch (error) {
         alert(error);
       } finally {
---
+++ web/src/service/ContactsService.js
@@ -0,0 +1,11 @@
+import delay from '../utils/delay';
+
+class ContactsService {
+  async listContacts(orderBy = 'asc') {
+    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+    await delay(500);
+    return response.json();
+  }
+}
+
+export default new ContactsService();
