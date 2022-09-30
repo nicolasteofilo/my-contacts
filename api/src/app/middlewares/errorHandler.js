b1e8f7da7a2455676e32e147dc05c975013351f9
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-09-29 21:50:42 -0300

    refactor: change message in handle error api

--- api/src/app/middlewares/errorHandler.js
+++ api/src/app/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const { AppError } = require('../../errors/AppError');
 
 module.exports = (err, request, response, next) => {
   if (err instanceof AppError) {
-    return response.status(err.statusCode).json({ message: err.message });
+    return response.status(err.statusCode).json({ error: err.message });
   }
 
   return response.status(500).json({
