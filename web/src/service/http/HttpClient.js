b6b7f44a520ff8664eb4b06969dc731724bff3a8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-09-29 21:51:09 -0300

    feat(web): parsing the body based on content-type

--- web/src/service/http/HttpClient.js
+++ web/src/service/http/HttpClient.js
@@ -9,11 +9,18 @@ class HttpClient {
     await delay(500);
 
     const response = await fetch(`${this.baseURL}${path}`);
-    if (response.ok) {
-      return response.json();;
+    const contentType =  response.headers.get('Content-Type');
+    let body = null;
+
+    if(contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(body?.error || `${response.status} - ${response.statusText}`);
   }
 
   post() {}
