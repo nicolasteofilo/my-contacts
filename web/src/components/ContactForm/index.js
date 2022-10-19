19bd0bac58d6cef4db630aa36bc122ecd2a7ab7e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-10-19 17:22:23 -0300

    feat(web): populating inputs in the contact edit screen imperatively

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 import PropTypes from 'prop-types';
 
 import isEmailValid from '../../utils/isEmailValid';
@@ -13,7 +13,7 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-export default function ContactForm({ buttonLabel, onSubmit }) {
+const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -26,6 +26,15 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     useErrors();
 
   const isFormValid = name && errors.length === 0;
+
+  useImperativeHandle(ref, () => ({
+      setFieldsValues: (contact) => {
+        setName(contact.name);
+        setEmail(contact.email);
+        setPhone(contact.phone);
+        setCategoryId(contact.category_id);
+      },
+    }), []);
 
   useEffect(() => {
     async function loadCategories() {
@@ -122,9 +131,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       </ButtonContainer>
     </Form>
   );
-}
+});
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
--- web/src/pages/EditContact/index.js
+++ web/src/pages/EditContact/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
@@ -10,6 +10,7 @@ import { toast } from '../../utils/toast';
 
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
 
   const { id } = useParams();
   const history = useHistory();
@@ -17,8 +18,8 @@ export default function EditContact() {
   useEffect(() => {
     async function loadContact() {
       try {
-        const contactData = await ContactsService.getContactById(id);
-        console.log({ contactData });
+        const contact = await ContactsService.getContactById(id);
+        contactFormRef.current.setFieldsValues(contact);
         setIsLoading(false);
       } catch (error) {
         history.push('/');
@@ -39,7 +40,11 @@ export default function EditContact() {
     <>
       <Loader isLoading={isLoading} />
       <PageHeader title="Editar contato" />
-      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
