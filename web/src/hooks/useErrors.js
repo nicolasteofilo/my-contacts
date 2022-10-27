ddea3575f725b49f8ae0f8faa12083d70fa135e9
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-10-27 14:51:10 -0300

    chore(web): eslint react hooks plugin

--- web/src/hooks/useErrors.js
+++ web/src/hooks/useErrors.js
@@ -1,24 +1,28 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
-    const errorAlreadyExists = errors.find((error) => error.field === field);
-    if (!errorAlreadyExists) {
-      setErrors((prevState) => [...prevState, { field, message }]);
-    }
-  }
+  const setError = useCallback(
+    ({ field, message }) => {
+      const errorAlreadyExists = errors.find((error) => error.field === field);
+      if (!errorAlreadyExists) {
+        setErrors((prevState) => [...prevState, { field, message }]);
+      }
+    },
+    [errors]
+  );
 
-  function removeError(fieldName) {
+  const removeError = useCallback((fieldName) => {
     setErrors((prevState) =>
       prevState.filter((error) => error.field !== fieldName)
     );
-  }
+  }, []);
 
-  function getErrorMessageByFieldName(fieldName) {
-    return errors.find((error) => error.field === fieldName)?.message;
-  }
+  const getErrorMessageByFieldName = useCallback(
+    (fieldName) => errors.find((error) => error.field === fieldName)?.message,
+    [errors]
+  );
 
   return { errors, setError, removeError, getErrorMessageByFieldName };
 }
