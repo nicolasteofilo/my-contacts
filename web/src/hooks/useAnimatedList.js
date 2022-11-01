b4d64314c9d296b25b094c3110b6f4e646b261d5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-11-01 18:00:17 -0300

    refactor(web): add listeners on animationend

--- web/src/hooks/useAnimatedList.js
+++ web/src/hooks/useAnimatedList.js
@@ -1,32 +1,52 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useRef, useState, useEffect } from 'react';
 
 export default function useAnimatedList(initialValue) {
   const [items, setItems] = useState(initialValue || []);
   const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([]);
 
+  console.log({ items, pendingRemovalItemsIds });
+
   const animatedRefs = useRef(new Map());
+  const animationEndListeners = useRef(new Map());
+
+  const handleAnimationEnd = useCallback((id) => {
+    setItems((prevState) => prevState.filter((message) => message.id !== id));
+    setPendingRemovalItemsIds((prevState) =>
+      prevState.filter((itemId) => itemId !== id)
+    );
+  }, []);
+
+  useEffect(() => {
+    pendingRemovalItemsIds.forEach((itemId) => {
+      const animatedRef = animatedRefs.current.get(itemId);
+      const alreadyHasListener = animationEndListeners.current.has(itemId);
+
+      if (animatedRef?.current && !alreadyHasListener) {
+        animationEndListeners.current.set(itemId, true);
+        animatedRef.current.addEventListener(
+          'animationend',
+          () => {
+            handleAnimationEnd(itemId);
+          }
+        );
+      }
+    });
+  }, [handleAnimationEnd, pendingRemovalItemsIds]);
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsIds((prevState) => [...prevState, id]);
   }, []);
 
-  // const handleAnimationEnd = useCallback((id) => {
-  //   setItems((prevState) => prevState.filter((message) => message.id !== id));
-  //   setPendingRemovalItemsIds((prevState) =>
-  //     prevState.filter((itemId) => itemId !== id)
-  //   );
-  // }, []);
-
   const getAnimatedRef = useCallback((id) => {
     let animatedElementRef = animatedRefs.current.get(id);
 
-    if(!animatedElementRef) {
-      animatedElementRef = { current: null } // or createRef()
+    if (!animatedElementRef) {
+      animatedElementRef = { current: null }; // or createRef()
       animatedRefs.current.set(id, animatedElementRef);
     }
 
     return animatedElementRef;
-  }, [])
+  }, []);
 
   const renderList = useCallback(
     (renderItem) =>
