b8502c27f75de80d33905fa7cbd14e7dda663bb5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-14 11:40:37 -0300

    feat(api): allowed multiple origins in cors

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,7 +1,15 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Allow-Age', '10');
+  const allowedOrigens = ['http://localhost:3000', 'http://localhost:3001'];
+  
+  const origin = request.header('origin');
+  const isAllowed = allowedOrigens.includes(origin)
+
+  if(isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Allow-Age', '10');
+  }
+
   next();
 }
